91b5bf748bda2ea049fcac679dea67c77bfbac4b
Sargun Vohra <sargunster@gmail.com>
2016-04-19 12:45:52 -0400

    Update colors.h

--- colors.h
+++ colors.h
@@ -1,23 +1,23 @@
 #pragma once
 
-#define RST  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
-#define KBOLD "\x1B[1m"
-#define KUNDL "\x1B[4m"
+#define RESET  "\x1B[0m"
+#define ESC_RED  "\x1B[31m"
+#define ESC_GREEN  "\x1B[32m"
+#define ESC_YELLOW  "\x1B[33m"
+#define ESC_BLUE  "\x1B[34m"
+#define ESC_MAGENTA  "\x1B[35m"
+#define ESC_CYAN  "\x1B[36m"
+#define ESC_WHITE  "\x1B[37m"
+#define ESC_BOLD "\x1B[1m"
+#define ESC_UNDERLINE "\x1B[4m"
 
-#define RED(x) KRED x RST
-#define GRN(x) KGRN x RST
-#define YEL(x) KYEL x RST
-#define BLU(x) KBLU x RST
-#define MAG(x) KMAG x RST
-#define CYN(x) KCYN x RST
-#define WHT(x) KWHT x RST
+#define RED(x) ESC_RED x RESET
+#define GREEN(x) ESC_GREEN x RESET
+#define YELLOW(x) ESC_YELLOW x RESET
+#define BLUE(x) ESC_BLUE x RESET
+#define MAGENTA(x) ESC_MAGENTA x RESET
+#define CYAN(x) ESC_CYAN x RESET
+#define WHITE(x) ESC_WHITE x RESET
 
-#define BOLD(x) KBOLD x RST
-#define UNDL(x) KUNDL x RST
+#define BOLD(x) ESC_BOLD x RESET
+#define UNDERLINE(x) ESC_UNDERLINE x RESET
